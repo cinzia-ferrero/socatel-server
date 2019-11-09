8daafcf9e84c33527b4d0586f3aabd9b741db50e
cinzia <cinzia_90@hotmail.it>
2019-11-09 11:34:33 +0100

    request body field to string + post

--- routes/services.js
+++ routes/services.js
@@ -6,24 +6,25 @@ const hospitalsData = require('./../data/hospitalsCL');
 const pharmaciesData = require('./../data/pharmaciesCL');
 const elderlyCareHomesData = require('./../data/elderlyCareHomesCL');
 
-router.get('/nearest', (req, res, next) => {
+router.post('/nearest', (req, res, next) => {
   const { lat, long, query } = req.body;
-  const numElements = req.body.numElementsPerCategory;
-  console.log(`GET ${numElements} nearest services to lat ${lat} long ${long} with query ${JSON.stringify(query)}`);
+  const numElements = parseInt(req.body.numElementsPerCategory);
+  const parsedQuery = JSON.parse(query);
+  console.log(`GET ${numElements} nearest services to lat ${lat} long ${long} with query ${JSON.stringify(parsedQuery)}`);
 
   let allNearest = [];
 
-  if(query.includes('hospital')) {
+  if(parsedQuery.includes('hospital')) {
     const nearestHospitals = helper._getNearest(hospitalsData, lat, long, numElements, 'hospital');
     allNearest = allNearest.concat(nearestHospitals);
   }
 
-  if(query.includes('pharmacie')) {
+  if(parsedQuery.includes('pharmacie')) {
     const nearestPharmacies = helper._getNearest(pharmaciesData, lat, long, numElements, 'pharmacie');
     allNearest = allNearest.concat(nearestPharmacies);
   }
 
-  if(query.includes('elderly_care_homes')) {
+  if(parsedQuery.includes('elderly_care_homes')) {
     const nearestElderlyCareHomes = helper._getNearest(elderlyCareHomesData, lat, long, numElements, 'elderlyCareHomes');
     allNearest = allNearest.concat(nearestElderlyCareHomes);
   }
